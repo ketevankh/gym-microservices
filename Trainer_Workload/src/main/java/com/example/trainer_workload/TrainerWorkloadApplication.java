76708c9f01de0ce2f38c7e23a9f9c3357655e69d
ketevankh <kkhac20@freeuni.edu.ge>
2024-05-14 16:38:32 +0400

    .

---
+++ Trainer_Workload/src/main/java/com/example/trainer_workload/TrainerWorkloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainer_workload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerWorkloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerWorkloadApplication.class, args);
+	}
+
+}
---
+++ Trainer_Workload/src/test/java/com/example/trainer_workload/TrainerWorkloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainer_workload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerWorkloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
