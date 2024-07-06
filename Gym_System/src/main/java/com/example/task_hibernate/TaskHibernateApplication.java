6efe2520b55dbaa5585b20f0b090baad75434b64
ketevankh <kkhac20@freeuni.edu.ge>
2024-07-06 13:02:05 +0400

    add activemq-broker dependency

--- Gym_System/src/main/java/com/example/task_hibernate/TaskHibernateApplication.java
+++ Gym_System/src/main/java/com/example/task_hibernate/TaskHibernateApplication.java
@@ -3,9 +3,6 @@ package com.example.task_hibernate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
